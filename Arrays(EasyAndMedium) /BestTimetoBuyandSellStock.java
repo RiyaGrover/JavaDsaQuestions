77d6999d9ee920f678ef8410959014e8eb7f501c
Riya Grover <riyagrover@Riyas-MacBook-Air.local>
2024-10-10 23:51:16 +0530

    begin again

---
+++ Arrays(EasyAndMedium) /BestTimetoBuyandSellStock.java
@@ -0,0 +1,18 @@
+//Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int mini= prices[0];
+        int profit = 0;
+
+        for(int i = 0; i < prices.length ;i++){
+            int cost = prices[i] - mini;
+            profit = Math.max(profit, cost);
+            mini = Math.min(mini , prices[i]);     
+             }
+        
+    return profit;
+}
+   
+}

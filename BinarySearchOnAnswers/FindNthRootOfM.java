8342303e7f24f2463fc169cd280589fae86ffcfa
Riya Grover <riyagrover@Riyas-MacBook-Air.local>
2024-11-25 16:22:11 +0530

    binarySearch

---
+++ BinarySearchOnAnswers/FindNthRootOfM.java
@@ -0,0 +1,58 @@
+// Link : https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find-nth-root-of-m
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int m = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.nthRoot(n, m);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int nthRoot(int n, int m) {
+        // code here
+        
+        int low = 1;
+        int high = m;
+        
+        while(low <= high){
+            int mid = (low+high)/2;
+             int midN = func(mid, n, m);
+            if(midN == 1 ){
+                return mid;
+            } else if (midN == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
+    public static int func(int mid, int n, int m) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if (ans > m) return 2;
+        }
+        if (ans == m) return 1;
+        return 0;
+    }
+}
---
+++ BinarySearchOnAnswers/FindSquareRootOfnumber.java
@@ -0,0 +1,23 @@
+// Link : https://www.geeksforgeeks.org/problems/square-root/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=square-root
+
+class Solution {
+    int floorSqrt(int n) {
+        // Your code here
+        
+        int low = 1;
+        int high = n;
+        int ans = 1;
+        while(low <= high){
+             int mid = (low+high)/2;
+            int sqa = mid*mid;
+            
+            if(sqa <= n ){
+                ans = mid;
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+}
